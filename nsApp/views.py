df7fa0d3076dcfbc9ab87312560961bd703c9b63
Mateusz <gmateusz92@gmail.com>
2024-04-03 15:26:49 +0200

    1

--- nsApp/views.py
+++ nsApp/views.py
@@ -1,10 +1,12 @@
 from .models import Author, Book
 from .serializers import AuthorSerializer, BooksSerializer
 from rest_framework import generics
+from rest_framework.pagination import PageNumberPagination
 
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
+    
 
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
@@ -13,6 +15,8 @@ class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
 class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
+    pagination_class = PageNumberPagination
+    pagination_class.page_size = 4
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()

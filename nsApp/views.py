b0ee18f6e77a8176e30a3eebef1469ec39925afe
Mateusz <gmateusz92@gmail.com>
2024-04-16 20:48:51 +0200

    1

--- nsApp/views.py
+++ nsApp/views.py
@@ -2,11 +2,13 @@ from .models import Author, Book
 from .serializers import AuthorSerializer, BooksSerializer
 from rest_framework import generics
 from rest_framework.pagination import PageNumberPagination
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework import filters
+
 
 class AuthorListView(generics.ListCreateAPIView):
     queryset = Author.objects.all()
     serializer_class = AuthorSerializer
-    
 
 class AuthorDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Author.objects.all()
@@ -16,7 +18,11 @@ class BookListView(generics.ListCreateAPIView):
     queryset = Book.objects.all()
     serializer_class = BooksSerializer
     pagination_class = PageNumberPagination
-    pagination_class.page_size = 4
+    pagination_class.page_size = 2
+    # filter_backends = [DjangoFilterBackend]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['id', 'ratings']
+    # filterset_fields = ['ratings']
 
 class BookDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Book.objects.all()
